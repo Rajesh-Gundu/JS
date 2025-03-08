dcadc97ce049764aa5e39fc316e221a6728a39b2
Rajesh <mail2rajeshgundu@gmail.com>
2025-03-08 19:09:43 +0530

    Add files via upload

---
+++ lesson3/exercises.js
@@ -0,0 +1,29 @@
+// 3a
+'My name is: '
+
+// 3b
+'Rajesh Gundu'
+
+// 3c
+'My name is: ' + 'Rajesh'
+
+// 3d
+'Total cost: $' + (5 + 3)
+
+// 3e
+`Total cost: $${5 + 3}`
+
+// 3f
+alert(`Total cost: $${5 + 3}`);
+
+// 3g
+'Total cost: $' + (599 + 295) / 100
+
+// 3h
+`Total cost: $${(599 + 295) / 100}`
+
+// 3i
+alert(`Total cost: $${(599 + 295) / 100}`);
+
+// 3j
+alert(`Total cost: $${(599 + 295) / 100}\nThank you, come again!`);
