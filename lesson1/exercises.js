569a818651fa4c8bde5f795d9579dbe8aac58ca3
Rajesh <mail2rajeshgundu@gmail.com>
2025-03-08 18:47:59 +0530

    Add files via upload

---
+++ lesson1/exercises.js
@@ -0,0 +1,20 @@
+// 1a
+alert("Good Morning!");
+
+// 1b
+alert("Rajesh Gundu");
+
+// 1c
+10 + 5
+
+// 1d
+20 - 5
+
+// 1e
+2 + 2 - 5
+
+// 1f
+document.body.innerHTML = 'Good morning!';
+
+// 1g
+document.body.innerHTML = "Rajesh Gundu";
